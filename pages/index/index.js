f2c9cd0613abd6838b734b72176f57c23746f2b9
johnhuichentmx <john.chen@tmx.com>
2019-01-08 20:08:21 -0500

    Use redux states in index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,20 @@
 import { connect } from 'wechat-weapp-redux'
 const app = getApp()
+const { store } = app
 
-const data = {
-  motto: 'Hello World',
-  canIUse: wx.canIUse('button.open-type.getUserInfo')
+function updateData() {
+  const {
+    user: { userInfo }
+  } = store.getState()
+  this.setData({ userInfo })
 }
 
 function onLoad() {
-  // console.log(this.data)
-  // if (app.globalData.userInfo) {
-  //   this.setData({
-  //     userInfo: app.globalData.userInfo,
-  //     hasUserInfo: true
-  //   })
-  // } else if (this.data.canIUse) {
-  //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-  //   // 所以此处加入 callback 以防止这种情况
-  //   app.userInfoReadyCallback = userInfo => {
-  //     this.setData({
-  //       userInfo,
-  //       hasUserInfo: true
-  //     })
-  //   }
-  // } else {
-  //   // 在没有 open-type=getUserInfo 版本的兼容处理
-  //   wx.getUserInfo({
-  //     success: res => {
-  //       app.globalData.userInfo = res.userInfo
-  //       this.setData({
-  //         userInfo: res.userInfo,
-  //         hasUserInfo: true
-  //       })
-  //     }
-  //   })
-  // }
+  this.unsubscribe = store.subscribe(() => this.updateData())
+}
+
+function onUnload() {
+  this.unsubscribe()
 }
 
 function bindViewTap() {
@@ -43,19 +24,15 @@ function bindViewTap() {
 }
 
 const mapStateToData = state => ({
-  userInfo: state.user.userInfo,
-  test: '123'
+  userInfo: state.user.userInfo
 })
 
-const mapDispatchToPage = dispatch => ({})
-
 const pageConfig = {
-  data,
+  data: { motto: 'Hello World' },
   bindViewTap,
-  onLoad
+  onLoad,
+  onUnload,
+  updateData
 }
-
-const test = connect(mapStateToData, mapDispatchToPage)(pageConfig)
-console.log(test)
 
 Page(connect(mapStateToData)(pageConfig))
